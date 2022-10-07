d93721b96809b3457d822985709618b43c17a329
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2022-10-07 12:42:50 +0100

    adding api and displaying results

--- main.js
+++ main.js
@@ -15,6 +15,32 @@ function searchMeal(e) {
   const term = search.value;
   //   Check for empty
   if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        resultHeading.innerHTML = `<h2>Results for '${term}' :</h2>`;
+
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no search results for '${term}'</p>`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meal) => `
+            <div class="meal">
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+              <div class="meal-info" data-mealID="${meal.idMeal}">
+                <h3>${meal.strMeal}</h3>
+              </div>
+            </div>
+          `
+            )
+            .join("");
+        }
+      });
+
+    // clear search text
+    search.value = " ";
   } else {
     alert("Please enter a search term");
   }
